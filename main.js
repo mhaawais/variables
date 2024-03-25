f96a491a7a2758f283e19782f88317124a691022
M Awais <awais23112001@gmail.com>
2024-03-25 11:51:18 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var uname = "Awais";
+var ulocation = "karachi";
+var marks = 690;
+var percentage = 80;
+console.log("name:", uname);
+console.log("location:", ulocation);
+console.log("marks:", marks);
+console.log("percentage:", percentage);
---
+++ main.ts
@@ -0,0 +1,10 @@
+
+var uname = "Awais"
+var ulocation : string = "karachi"
+var marks : number = 690
+var percentage : number = 80
+
+console.log("name:",uname)
+console.log("location:",ulocation)
+console.log("marks:",marks)
+console.log("percentage:",percentage)
